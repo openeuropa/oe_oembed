50412083487df804b74547abe58dc86ffc260296
Imanol <ier@opendeusto.es>
2019-06-04 12:15:37 +0100

    OPENEUROPA-1910: Use the OembedResolver on the oEmbed route.

--- src/Controller/OembedRouteController.php
+++ src/Controller/OembedRouteController.php
@@ -7,7 +7,11 @@ namespace Drupal\oe_oembed\Controller;
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Access\AccessResultInterface;
 use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Url;
+use Drupal\oe_oembed\Oembed\OembedResolver;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 /**
  * Oembed route controller.
@@ -15,15 +19,38 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 class OembedRouteController {
 
   /**
+   * The oEmbed resolver.
+   *
+   * @var \Drupal\oe_oembed\Oembed\OembedResolver
+   */
+  protected $oembedResolver;
+
+  /**
+   * Constructs the route controller.
+   *
+   * @param \Drupal\oe_oembed\Oembed\OembedResolver $oembed_resolver
+   *   The entity repository.
+   */
+  public function __construct(OembedResolver $oembed_resolver) {
+    $this->oembedResolver = $oembed_resolver;
+  }
+
+  /**
    * Returns the oEmbed json object related to the request.
+   *
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   The received request.
    *
    * @return \Symfony\Component\HttpFoundation\JsonResponse
    *   The JSON response.
    */
-  public function getOembedJson(): JsonResponse {
-    return new JsonResponse([
-      'status' => 'OK',
-    ]);
+  public function getOembedJson(Request $request): JsonResponse {
+    $query_parameters = $request->query->all();
+    if (!isset($query_parameters['url'])) {
+      throw new BadRequestHttpException('Url parameter not found.');
+    }
+    $json_array = $this->oembedResolver->resolve(Url::fromUri($query_parameters['url']));
+    return new JsonResponse($json_array);
   }
 
   /**
