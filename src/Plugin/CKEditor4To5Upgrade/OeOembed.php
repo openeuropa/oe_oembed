9a0a1d5fa4a4045fd483c4cbf975176382129d59
Francesco Sardara <francesco@tdgwebservices.com>
2023-09-08 23:54:03 +0200

    EWPP-3249: Add migration.

---
+++ src/Plugin/CKEditor4To5Upgrade/OeOembed.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\oe_oembed\Plugin\CKEditor4To5Upgrade;
+
+use Drupal\ckeditor5\HTMLRestrictions;
+use Drupal\ckeditor5\Plugin\CKEditor4To5UpgradePluginInterface;
+use Drupal\Core\Plugin\PluginBase;
+use Drupal\filter\FilterFormatInterface;
+
+/**
+ * Provides the upgrade from CKEditor 4 to 5.
+ *
+ * @CKEditor4To5Upgrade(
+ *   id = "oe_oembed",
+ *   cke4_buttons = {
+ *   },
+ *   cke4_plugin_settings = {
+ *   },
+ *   cke5_plugin_elements_subset_configuration = {
+ *   }
+ * )
+ */
+class OeOembed extends PluginBase implements CKEditor4To5UpgradePluginInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function mapCKEditor4ToolbarButtonToCKEditor5ToolbarItem(string $cke4_button, HTMLRestrictions $text_format_html_restrictions): ?array {
+    // Buttons are equally named in CKEditor 4 and 5.
+    return [$cke4_button];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function mapCKEditor4SettingsToCKEditor5Configuration(string $cke4_plugin_id, array $cke4_plugin_settings): ?array {
+    throw new \OutOfBoundsException();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function computeCKEditor5PluginSubsetConfiguration(string $cke5_plugin_id, FilterFormatInterface $text_format): ?array {
+    throw new \OutOfBoundsException();
+  }
+
+}
