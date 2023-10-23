2a3a563b111bb4a39ca923f46085c05685122583
Francesco Sardara <francesco@tdgwebservices.com>
2023-10-23 14:26:23 +0200

    EWPP-3249: Make sure that buttons still exist when executing CK4 to 5 migrations.

--- src/Plugin/CKEditor4To5Upgrade/OeOembed.php
+++ src/Plugin/CKEditor4To5Upgrade/OeOembed.php
@@ -6,8 +6,11 @@ namespace Drupal\oe_oembed\Plugin\CKEditor4To5Upgrade;
 
 use Drupal\ckeditor5\HTMLRestrictions;
 use Drupal\ckeditor5\Plugin\CKEditor4To5UpgradePluginInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Plugin\PluginBase;
 use Drupal\filter\FilterFormatInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides the upgrade from CKEditor 4 to 5.
@@ -24,14 +27,63 @@ use Drupal\filter\FilterFormatInterface;
  *
  * phpcs:disable Drupal.NamingConventions.ValidFunctionName.ScopeNotCamelCaps
  */
-class OeOembed extends PluginBase implements CKEditor4To5UpgradePluginInterface {
+class OeOembed extends PluginBase implements CKEditor4To5UpgradePluginInterface, ContainerFactoryPluginInterface {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected EntityTypeManagerInterface $entityTypeManager;
+
+  /**
+   * Constructs a new instance of this class.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
+   *   The entity type manager.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entityTypeManager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+
+    $this->entityTypeManager = $entityTypeManager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager')
+    );
+  }
 
   /**
    * {@inheritdoc}
    */
   public function mapCKEditor4ToolbarButtonToCKEditor5ToolbarItem(string $cke4_button, HTMLRestrictions $text_format_html_restrictions): ?array {
-    // Buttons are equally named in CKEditor 4 and 5.
-    return [$cke4_button];
+    // Buttons have the same ID in CKEditor 4 and 5.
+    $embed_buttons = $this->entityTypeManager
+      ->getStorage('embed_button')
+      // @see \Drupal\entity_embed\Plugin\EmbedType\Entity
+      ->loadByProperties([
+        'type_id' => 'oe_oembed_entities',
+        'id' => $cke4_button,
+      ]);
+
+    if (!empty($embed_buttons)) {
+      return [$cke4_button];
+    }
+
+    throw new \OutOfBoundsException();
   }
 
   /**
